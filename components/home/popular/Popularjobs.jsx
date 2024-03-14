b82682fed0401e21cfde53bac6bbc73986e71b93
Santajit Patra <121734312+santajitpatra@users.noreply.github.com>
2024-03-14 23:56:41 +0530

    feat: Implement useFetch hook for API data fetching
    feat: Implement useFetch hook for API data fetching
    
    This commit introduces the `useFetch` custom hook to simplify API data fetching in React components.
    
    * Handles loading states, errors, and data refreshing.
    * Utilizes axios for making HTTP requests.

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { useState } from "react";
 import { useRouter } from "expo-router";
 import {
@@ -12,11 +11,16 @@ import {
 import styles from "./popularjobs.style";
 import { COLORS, SIZES } from "../../../constants";
 import PopularJobCard from "../../common/cards/popular/PopularJobCard";
+import { useFetch } from "../../../hook/useFetch"
 
 const Popularjobs = () => {
   const router = useRouter();
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [error, setError] = useState(false);
+    const { data, isLoading, error } = useFetch("search", {
+      query: "React developer",
+      num_pages: "1",
+    });
 
   return (
     <View style={styles.container}>
@@ -34,13 +38,8 @@ const Popularjobs = () => {
           <Text>Something went wrong</Text>
         ) : (
           <FlatList
-            data={[1,2,3,4,5,6,7,8,9,10]}
-            renderItem={({ item }) => (
-              <PopularJobCard
-                item={item}
-              
-              />
-            )}
+            data={data}
+            renderItem={({ item }) => <PopularJobCard item={item} />}
             keyExtractor={(item) => item.job_id}
             contentContainerStyle={{ columnGap: SIZES.medium }}
             horizontal
